8f6130d3a877c029eee5fbfe8d40ae95457d8370
Alok <alokprajapati0303@gmail.com>
2025-08-21 02:07:39 +0530

    just small changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/project4/">
       <App />
     </BrowserRouter>
   </StrictMode>,
